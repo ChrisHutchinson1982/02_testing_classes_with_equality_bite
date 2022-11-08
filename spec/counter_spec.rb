2f723f42898d120336e63d19f6c85739cc8a43e1
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-08 14:13:08 +0000

    Refactored tests to counter class

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -3,22 +3,18 @@ require 'counter'
 RSpec.describe Counter do
   it "report empty counter" do
     counter = Counter.new
-    result = counter.report
-    expect(result).to eq "Counted to 0 so far."
+    expect(counter.report).to eq "Counted to 0 so far."
   end
   it "add 3 to counter" do
     counter = Counter.new
     counter.add(3)
-    result = counter.report
-    expect(result).to eq "Counted to 3 so far."
+    expect(counter.report).to eq "Counted to 3 so far."
   end
   it "add 3 then 4 to counter" do
     counter = Counter.new
     counter.add(3)
-    result = counter.report
-    expect(result).to eq "Counted to 3 so far."
+    expect(counter.report).to eq "Counted to 3 so far."
     counter.add(4)
-    result = counter.report
-    expect(result).to eq "Counted to 7 so far."
+    expect(counter.report).to eq "Counted to 7 so far."
   end
 end
